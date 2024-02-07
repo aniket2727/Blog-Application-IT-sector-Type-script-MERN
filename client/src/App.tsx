626f7f6300796b3383f03134a4ecb95ac146d0a4
aniket2727 <aniketkadam5950@gmail.com>
2024-02-07 09:32:44 +0530

    installled and created browser routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';}
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+      </BrowserRouter>
          
     </div>
   );
