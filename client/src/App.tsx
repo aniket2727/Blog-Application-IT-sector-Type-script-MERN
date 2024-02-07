041a65f9640a3cd90e75e1d4ffedd17848437e80
aniket2727 <aniketkadam5950@gmail.com>
2024-02-07 09:38:58 +0530

    added home componets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';}
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import HomeComponents from './componets/HomeComponets';
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/home"  element={<HomeComponents/>}/>
       </Routes>
       </BrowserRouter>
          
---
+++ client/src/componets/HomeComponets.tsx
@@ -0,0 +1,15 @@
+// HomeComponets.tsx
+import React from 'react';
+
+const HomeComponents: React.FC = () => {
+  return (
+    <div>
+        <h1  className='text-blue-400 text-center'>this is home home componets</h1>
+    </div>
+  );
+};
+
+export default HomeComponents;
+
+// Add an empty export statement to make it a module
+export {};
